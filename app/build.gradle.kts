ac0c90299eb01d35a2e3652fc52cf12e59c685d2
MESUT GOK <mesutcukgok@gmail.com>
2026-03-27 01:59:21 +0200

    Add section header for test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
-    // Tests
+    // --- Tests --- 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
